f3f5174e32badbcac0ddc1940559f7b8968a5187
dugehui <53008870+dugehui@users.noreply.github.com>
2019-10-23 05:23:47 +0800

    Add files via upload

--- __tests__/discipleTests.js
+++ __tests__/discipleTests.js
@@ -1,1 +1,62 @@
+import Disciple from "../disciple"
+import { AssertionError } from "assert"
 
+describe('Disciple', () => {
+    const disciple = new Disciple('test')
+
+    describe('eatMushrooms', () => {
+
+        it('mushroomsAte smaller than 3, stillAlive', () => {
+            disciple.mushroomsAte = 1
+            disciple.eatMushrooms()
+            expect(disciple.mushroomsAte).toBe(2)
+            expect(disciple.isStillAlive).toBeTruthy()
+        })
+
+        it('mushroomsAte bigger than 3, notAlive', () => {
+            disciple.mushroomsAte = 5
+            disciple.eatMushrooms()
+            expect(disciple.mushroomsAte).toBe(6)
+            expect(disciple.isStillAlive).toBeFalsy()
+            expect(disciple.eatMushrooms()).toBe(`test screams and dies in agony.`)
+        })
+
+        it('mushroomsAte is 3, notAlive', () => {
+            disciple.mushroomsAte = 3
+            // disciple.name = 'aname'
+            disciple.eatMushrooms()
+            expect(disciple.mushroomsAte).toBe(4)
+            expect(disciple.isStillAlive).toBeFalsy()
+            expect(disciple.eatMushrooms()).toBe(`test screams and dies in agony.`)
+        })
+    })
+    describe('isTrained', () => {
+        it('false, mushroomsAte!==0, trainingSessions=0', () => {
+            disciple.mushroomsAte = 3
+            disciple.trainingSessions = 0
+            expect(disciple.isTrained()).toBeFalsy()
+        })
+        it('false, mushroomsAte == 0, trainingSessions = 2', () => {
+            disciple.mushroomsAte = 0
+            disciple.trainingSessions = 2
+            expect(disciple.isTrained()).toBeFalsy()
+        })
+        it('false, mushroomsAte == 0, trainingSessions = 0', () => {
+            disciple.mushroomsAte = 0
+            disciple.trainingSessions = 0
+            expect(disciple.isTrained()).toBeFalsy()
+        })
+        it('true, mushroomsAte !== 0, trainingSessions = 2', () => {
+            disciple.mushroomsAte = 3
+            disciple.trainingSessions = 2
+            expect(disciple.isTrained()).toBeTruthy()
+        })
+    })
+    describe('train', () => {
+        it('trainingSessions', () => {
+            disciple.trainingSessions = 3
+            disciple.train()
+            expect(disciple.trainingSessions).toBe(4)
+        })
+    })
+})
